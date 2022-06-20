8c6793af50d35281da1da310c73707f4ccc4e475
adriel vinicius <adriel1234.1234@gmail.com>
2022-06-20 15:42:45 -0400

    Validação dos campos digitados pelo usuario

--- src/main/java/com/eventoapp/models/Convidado.java
+++ src/main/java/com/eventoapp/models/Convidado.java
@@ -1,14 +1,20 @@
 package com.eventoapp.models;
 
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+
+import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 public class Convidado {
 	
 	@Id
+	@NotEmpty
 	private String rg;
+	
+	@NotEmpty
 	private String nomeConvidado;
 	
 	@ManyToOne
