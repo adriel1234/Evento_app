ada1262dd023636df1bac7224c240dc5daaa6c31
adriel vinicius <adriel1234.1234@gmail.com>
2022-06-20 14:32:27 -0400

    Salvando convidados no bando de dados

--- src/main/java/com/eventoapp/models/Convidado.java
+++ src/main/java/com/eventoapp/models/Convidado.java
@@ -13,24 +13,30 @@ public class Convidado {
 	
 	@ManyToOne
 	private Evento evento;
-	
-	public Evento getEvento() {
-		return evento;
-	}
-	public void setEvento(Evento evento) {
-		this.evento = evento;
-	}
+
 	public String getRg() {
 		return rg;
 	}
+
 	public void setRg(String rg) {
 		this.rg = rg;
 	}
-	public String getNomeconvidado() {
+
+	public String getNomeConvidado() {
 		return nomeConvidado;
 	}
-	public void setNomeconvidado(String nomeConvidado) {
+
+	public void setNomeConvidado(String nomeConvidado) {
 		this.nomeConvidado = nomeConvidado;
 	}
+
+	public Evento getEvento() {
+		return evento;
+	}
+
+	public void setEvento(Evento evento) {
+		this.evento = evento;
+	}
+	
 
 }
